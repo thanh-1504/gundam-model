7a5ccbdee32a6f1076e22f7220e3927204386d35
nhatthanh <duongnhatthanhaz09@gmail.com>
2026-02-27 20:23:13 +0700

    Test api

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("http://localhost:3000");
-      const data = await response.json();
-      console.log(data);
+      const response = await fetch("https://gundam-model.onrender.com/");
+      const result = await response.json();
+      setData(result);
     }
     fetchData();
   }, []);
+  console.log(data.data);
 
   return (
     <>
       <h1>Hello World with Vite</h1>
+      {data?.data?.length > 0 &&
+        data.data.map((item) => {
+          return <p>{item.name}</p>;
+        })}
     </>
   );
 }
